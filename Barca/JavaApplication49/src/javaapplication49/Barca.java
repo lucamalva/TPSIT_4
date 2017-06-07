280034e5f652cdbee71a6c0d9fac05ff7ea5c7ab
lucamalva <malvataniluca@gmail.com>
2017-06-07 12:40:12 +0200

    Add files via upload

---
+++ Barca/JavaApplication49/src/javaapplication49/Barca.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication49;
+
+/**
+ *
+ * @author Studente
+ */
+public class Barca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Finestra f= new Finestra();
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+        // TODO code application logic here
+    }
+    
+}
